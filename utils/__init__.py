bc6c1174b65c01ffe46095dd1b3c6d9b99b57ef1
Yangshan Xiang <yangshan.xiang@stud.uni-goettingen.de>
2024-10-29 21:08:21 +0100

    Recalled the structure of CNN

--- utils/__init__.py
+++ utils/__init__.py
@@ -14,6 +14,28 @@ import torchvision
 from torch.utils.data import DataLoader
 
 
+# DO NOT CHANGE
+def get_device(cuda_preference=True):
+    """Gets pytorch device object. If cuda_preference=True and
+        cuda is available on your system, returns a cuda device.
+
+    Args:
+        cuda_preference: bool, default True
+            Set to true if you would like to get a cuda device
+
+    Returns: pytorch device object
+            Pytorch device
+    """
+
+    print('cuda available:', torch.cuda.is_available(),
+          '; cudnn available:', torch.backends.cudnn.is_available(),
+          '; num devices:', torch.cuda.device_count())
+
+    use_cuda = False if not cuda_preference else torch.cuda.is_available()
+    device = torch.device('cuda:0' if use_cuda else 'cpu')
+    device_name = torch.cuda.get_device_name(device) if use_cuda else 'cpu'
+    print('Using device', device_name)
+    return device
 
 # DO NOT CHANGE
 def grab_data(data_dir, num_cpus=1):
