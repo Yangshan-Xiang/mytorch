1aa3e498bc9819a4702b6b5bcb5d308953903777
Yangshan Xiang <yangshan.xiang@stud.uni-goettingen.de>
2025-05-02 18:55:35 +0200

    Implementing the HT model.

--- HT.py
+++ HT.py
@@ -1,1 +1,36 @@
-# I am going to try
+import numpy as np
+
+class HT:
+    """
+    Initialize the Hierarchical Tucker (HT) model presented in section 3.2 of the paper.
+
+    Args:
+
+    Returns:
+
+
+    """
+    def __init__(self, n, m, y, ranks):
+        self.N = n
+        self.M = m
+        self.Y = y
+        self.ranks = ranks
+        self.L = int(np.log2(n))
+        self.params = {'level_0': np.random.randn(n, ranks[0], m)}
+        for l in range(1, self.L):
+            self.params[f'level_l'] = np.random.randn(n // (2**l), ranks[l], ranks[l-1])
+        self.params['final'] = np.random.randn(y, ranks[-1])
+
+    def forward(self, X):
+        """
+        Forward pass of the HT model.
+
+        Args:
+
+        Returns:
+
+        """
+        
+
+
+
