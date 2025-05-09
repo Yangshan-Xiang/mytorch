3275619c9e91bec9df254764c93f7d2dc75038da
Yangshan Xiang <yangshan.xiang@stud.uni-goettingen.de>
2025-05-09 23:16:44 +0200

    Implemented the computation of the final coefficient tensor A, next is to compute the score function together with input X.

--- models/HTModel.py
+++ models/HTModel.py
@@ -1,10 +1,12 @@
 import numpy as np
 
-# The key to implement this model is how to index and structure all the parameters such that they can be computed recursively
+# The key to implement this model is how to index and structure all the parameters such that they can
+# be computed recursively
 class HTModel():
     def __init__(self, N, M, ranks, Y):
         # The notations are consistent with the notations used in the original paper
-        if not isinstance(N, int) or not isinstance(M, int) or not isinstance(Y, int) or not (isinstance(r, int) for r in ranks):
+        if (not isinstance(N, int) or not isinstance(M, int) or not isinstance(Y, int)
+                or not (isinstance(r, int) for r in ranks)):
             raise TypeError("N, M, ranks and Y must be integers.")
         if N < 1 or M < 1 or Y < 1 or (r < 1 for r in ranks):
             raise ValueError("N, M, ranks and Y must be positive.")
@@ -24,21 +26,34 @@ class HTModel():
         np.random.seed(42)
         self.params = [np.random.randn(N, ranks[0], M)] # The parameters of the 1st hidden-layer
         for l in range(1, self.L):
-            self.params.append(np.random.randn(N // 2 ** l, ranks[l], ranks[l-1])) # The parameters of the rest hidden-layers
-        self.params.append(np.random.randn(1, Y, ranks[-1])) # The parameters of the output layer.
+            self.params.append(np.random.randn(N // 2 ** l, ranks[l], ranks[l-1])) # The parameters of the
+            # rest hidden-layers
+        self.params.append(np.random.randn(Y, ranks[-1])) # The parameters of the output layer.
 
     def forward(self, X): # X is the input instance which has the size of (N, s)
         # The task here is to compute the phi tensors hierarchically according to the equation (4) in the paper
         phis = [self.params[0]] # phi_0 tensor is just the stack of parameter vectors in the 1st hidden-layer
         for l in range (1, self.L):
-            phi = np.zeros((self.N // 2 ** l, self.ranks[l], self.M ** 2 ** l)) # Compute the intermediate level phi tensors hierarchically
+            phi = np.zeros((self.N // 2 ** l, self.ranks[l], self.M ** 2 ** l)) # Compute the intermediate level
+            # phi tensors hierarchically
             # Compute the phi_l tensor by computing each phi_l_j_gamma recursively
             for j in range(self.N // 2 ** l):
                 for gamma in range(self.ranks[l]):
                     for alpha in range(self.ranks[l-1]):
-                        # This line of code EXACTLY matches the expression of phi_l_j_gamma depicted in the equation (4)
-                        phi[j, gamma] += np.kron(phis[l-1][2 * j, alpha], phis[l-1][2 * j + 1, alpha]) * self.params[l][j, gamma, alpha]
+                        # This line of code EXACTLY matches the expression of phi_l_j_gamma depicted in
+                        # the equation (4)
+                        phi[j, gamma] += (np.kron(phis[l-1][2 * j, alpha], phis[l-1][2 * j + 1, alpha]) *
+                                          self.params[l][j, gamma, alpha])
             phis.append(phi)
+        # Compute the final coefficient tensor A_y using the phi tensors
+        A = np.zeros((self.Y, self.M ** 2 ** self.L))
+        for y in range(self.Y):
+            for alpha in range(self.ranks[-1]):
+                # Again this EXACTLY matches the expression of A_y depicted in the equation (4)
+                A[y] += np.kron(phis[-1][0, alpha], phis[-1][1, alpha]) * self.params[-1][y, alpha]
+
+
+
 
 
 
