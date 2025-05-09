5ccf7c2aa39df4acf2793d7b091eedf563b86a1d
Yangshan Xiang <yangshan.xiang@stud.uni-goettingen.de>
2025-05-09 22:53:48 +0200

    Implemented the computation of the phi tensors of each layer, next step is to add the final output layer.

--- models/HTModel.py
+++ models/HTModel.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# The key to implement this model is how to index and structure all the parameters such that they can be computed recursively
 class HTModel():
     def __init__(self, N, M, ranks, Y):
         # The notations are consistent with the notations used in the original paper
@@ -26,6 +27,20 @@ class HTModel():
             self.params.append(np.random.randn(N // 2 ** l, ranks[l], ranks[l-1])) # The parameters of the rest hidden-layers
         self.params.append(np.random.randn(1, Y, ranks[-1])) # The parameters of the output layer.
 
-    def forward(self, X):
+    def forward(self, X): # X is the input instance which has the size of (N, s)
+        # The task here is to compute the phi tensors hierarchically according to the equation (4) in the paper
+        phis = [self.params[0]] # phi_0 tensor is just the stack of parameter vectors in the 1st hidden-layer
+        for l in range (1, self.L):
+            phi = np.zeros((self.N // 2 ** l, self.ranks[l], self.M ** 2 ** l)) # Compute the intermediate level phi tensors hierarchically
+            # Compute the phi_l tensor by computing each phi_l_j_gamma recursively
+            for j in range(self.N // 2 ** l):
+                for gamma in range(self.ranks[l]):
+                    for alpha in range(self.ranks[l-1]):
+                        # This line of code EXACTLY matches the expression of phi_l_j_gamma depicted in the equation (4)
+                        phi[j, gamma] += np.kron(phis[l-1][2 * j, alpha], phis[l-1][2 * j + 1, alpha]) * self.params[l][j, gamma, alpha]
+            phis.append(phi)
+
+
+
 
 
