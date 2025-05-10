9d9c22b8d61fa2152eca887535f0b55c700ddb55
Yangshan Xiang <yangshan.xiang@stud.uni-goettingen.de>
2025-05-10 19:25:19 +0200

    Successfully implemented the HT model depicted in the section 3.2 equation (4) and figure 2, although the model parameters can not be trained and are randomly initialized, the idea of this is to get more familiar to the structure of the HT model.

--- models/HTModel.py
+++ models/HTModel.py
@@ -3,27 +3,26 @@ import numpy as np
 # The key to implement this model is how to index and structure all the parameters such that they can
 # be computed recursively
 class HTModel():
-    def __init__(self, N, M, ranks, Y):
+    def __init__(self, N:int, M:int, ranks:list, Y:int):
         # The notations are consistent with the notations used in the original paper
         if (not isinstance(N, int) or not isinstance(M, int) or not isinstance(Y, int)
-                or not (isinstance(r, int) for r in ranks)):
+                or not all(isinstance(r, int) for r in ranks)):
             raise TypeError("N, M, ranks and Y must be integers.")
-        if N < 1 or M < 1 or Y < 1 or (r < 1 for r in ranks):
+        if N < 1 or M < 1 or Y < 1 or all(r < 1 for r in ranks):
             raise ValueError("N, M, ranks and Y must be positive.")
         if (N & (N - 1)) != 0:
             raise ValueError("N must be the power of 2.")
         if len(ranks) != np.log2(N):
-            raise ValueError(f"The number of ranks must equal to {np.log2(N)}.")
+            raise ValueError(f"The number of ranks must equal to {int(np.log2(N))}.")
 
         self.N = N # The number of input data, the number of vectorized patches of one input image, need to be
         # the power of 2 for simplicity as described in the paper
         self.M = M # The number of representation functions, the number of input channels
-        self.ranks = ranks # The ranks of the HT decomposition, the number of channels in each feature map
+        self.ranks = ranks # A list of ranks of the HT decomposition, the number of channels in each feature map
         self.Y = Y # The number of output classes, the number of channels in the output layer
-        self.L = np.log2(N) # The number of hidden-layers, exclude the representation layer and output layer
+        self.L = int(np.log2(N)) # The number of hidden-layers, exclude the representation layer and output layer
 
         # Define the parameters, they are randomly initialized and NOT trainable now!
-        np.random.seed(42)
         self.params = [np.random.randn(N, ranks[0], M)] # The parameters of the 1st hidden-layer
         for l in range(1, self.L):
             self.params.append(np.random.randn(N // 2 ** l, ranks[l], ranks[l-1])) # The parameters of the
@@ -31,6 +30,10 @@ class HTModel():
         self.params.append(np.random.randn(Y, ranks[-1])) # The parameters of the output layer.
 
     def forward(self, X): # X is the input instance which has the size of (N, s)
+        # Representation layer, will convert the shape of X into (N, M)
+        weights = np.random.randn(X.shape[-1], self.M)
+        F = X @ weights
+
         # The task here is to compute the phi tensors hierarchically according to the equation (4) in the paper
         phis = [self.params[0]] # phi_0 tensor is just the stack of parameter vectors in the 1st hidden-layer
         for l in range (1, self.L):
@@ -46,16 +49,22 @@ class HTModel():
                                           self.params[l][j, gamma, alpha])
             phis.append(phi)
         # Compute the final coefficient tensor A_y using the phi tensors
-        A = np.zeros((self.Y, self.M ** 2 ** self.L))
+        A = np.zeros((self.Y, * [self.M] * self.N)) # Evert A_y tensor has the shape of M ^ N
         for y in range(self.Y):
             for alpha in range(self.ranks[-1]):
                 # Again this EXACTLY matches the expression of A_y depicted in the equation (4)
-                A[y] += np.kron(phis[-1][0, alpha], phis[-1][1, alpha]) * self.params[-1][y, alpha]
+                A[y] += (np.kron(phis[-1][0, alpha], phis[-1][1, alpha]).reshape(* [self.M] * self.N)
+                         * self.params[-1][y, alpha]) # Reshaped to M ^ N
 
+        # Compute the score function h_y(X) depicted in function (2) in the paper
+        h = np.zeros(self.Y)
 
+        # Prepare the factor which can be directly multiplied with tensor A_y
+        factor = np.ones(A[0].shape)
+        for i in range(self.N):
+            factor *= F[i, np.indices(A[0].shape)[i]]
 
+        for y in range(self.Y):
+            h[y] = np.sum(A[y] * factor) # Multiply A_y with the factor and sum up all the elements
 
-
-
-
-
+        return h
