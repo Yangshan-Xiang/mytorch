2a64f435766a0f1f43166570e92cadf243015f46
Yangshan Xiang <yangshan.xiang@stud.uni-goettingen.de>
2025-05-11 01:54:56 +0200

    Restored tests.py file.

--- tests.py
+++ tests.py
@@ -1,13 +1,11 @@
 import unittest
 import numpy as np
-import torch
-from models.HTModel import HTModel
+from models.HTModelFixed import HTModelFixed
 
 class TestFunctions(unittest.TestCase):
 
     def test_HTModelFixed(self):
-        self.assertEqual(HTModel(4, 2, 2, 10, [12, 24]).forward(torch.randn(64, 4, 2)).shape[0], 64)
-        self.assertEqual(HTModel(4, 2, 2, 10, [12, 24]).forward(torch.randn(64, 4, 2)).shape[1], 10)
+        self.assertEqual(HTModelFixed(4, 2, 10, [12, 24]).forward(np.random.randn(4, 2)).shape[0], 10)
 
 
 if __name__ == '__main__':
